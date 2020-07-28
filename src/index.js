dd848643970c69b4ae559cc348d6fbe47e38983f
Carlos Eduardo Martins Filho <carlos.edumart@outlook.com>
2020-07-28 20:41:42 -0300

    ta dificil

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
-const jimp = require("../src/api/jimp");
 const fs = require("fs");
 const path = require("path");
 
@@ -10,11 +9,7 @@ const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 bot.queues = new Map();
 
-bot.on('guildMemberAdd', async membro =>{
-  const canal = bot.channels.cache.get("717472002759458939");
-  await jimp.imagemGen(membro.user.username);
-  canal.send(``,{files: ["../wikipotatobot/src/imgs/bemvindo.jpg"]});
-});
+
 
 const commandFiles = fs
   .readdirSync(path.join(__dirname, "/commands"))
